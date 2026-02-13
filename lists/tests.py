b40cb94ab744a1411adfddbc54bfe8f125a88396
Mistyhops <nedgalkin@gmail.com>
2026-02-13 11:43:51 +0300

    add form & post request

--- lists/tests.py
+++ lists/tests.py
@@ -8,7 +8,13 @@ from .views import home_page
 class HomePageTest(TestCase):
     """Тест домашней страницы"""
 
-    def test_home_page_returns_correct_html(self):
-        """Тест: домашняя страница возвращает правильный html"""
+    def test_uses_home_template(self):
+        """Тест: используется домашний шаблон"""
         response = self.client.get('/')
+        self.assertTemplateUsed(response, 'home.html')
+
+    def test_can_save_a_POST_request(self):
+        """Тест: можно сохранить post-запрос"""
+        response = self.client.post('/', data={'item_text': 'A new list item'})
+        self.assertIn('A new list item', response.content.decode())
         self.assertTemplateUsed(response, 'home.html')
--- lists/views.py
+++ lists/views.py
@@ -1,7 +1,8 @@
+from django.core.handlers.wsgi import WSGIRequest
 from django.http import HttpResponse
 from django.shortcuts import render
 
 
-def home_page(request):
+def home_page(request: WSGIRequest):
     """Домашняя страница"""
-    return render(request, 'home.html')
+    return render(request, 'home.html', {'new_item_text': request.POST.get('item_text', '')})
