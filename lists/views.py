6adfe46c75c307717d074cd5760414bc61bdb0ce
Mistyhops <nedgalkin@gmail.com>
2026-02-16 16:00:42 +0300

    addd new list view

--- lists/views.py
+++ lists/views.py
@@ -7,10 +7,6 @@ from lists.models import Item
 
 def home_page(request: WSGIRequest):
     """Домашняя страница"""
-    if request.method == 'POST':
-        new_item_text = request.POST['item_text']
-        Item.objects.create(text=new_item_text)
-        return redirect('/lists/the-only-one-list/')
     return render(
         request,
         'home.html',
@@ -21,3 +17,9 @@ def view_list(request: WSGIRequest):
     """Представление списка"""
     items = Item.objects.all()
     return render(request, 'list.html', {'items': items})
+
+
+def new_list(request: WSGIRequest):
+    """Новый список"""
+    Item.objects.create(text=request.POST['item_text'])
+    return redirect('/lists/the-only-one-list/')
