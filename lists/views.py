e2c823a1fc2032700b693ed6e8cfa552d8c64af9
Mistyhops <nedgalkin@gmail.com>
2026-02-17 16:15:23 +0300

    add view to add items to list

--- lists/views.py
+++ lists/views.py
@@ -13,14 +13,21 @@ def home_page(request: WSGIRequest):
     )
 
 
-def view_list(request: WSGIRequest):
+def view_list(request: WSGIRequest, list_id: int):
     """Представление списка"""
-    items = Item.objects.all()
-    return render(request, 'list.html', {'items': items})
+    list_ = List.objects.get(id=list_id)
+    return render(request, 'list.html', {'list': list_})
 
 
 def new_list(request: WSGIRequest):
     """Новый список"""
     list_ = List.objects.create()
     Item.objects.create(text=request.POST['item_text'], list=list_)
-    return redirect('/lists/the-only-one-list/')
+    return redirect(f'/lists/{list_.id}/')
+
+
+def add_item(request: WSGIRequest, list_id: int):
+    """Добавить элемент в список"""
+    list_ = List.objects.get(id=list_id)
+    Item.objects.create(text=request.POST["item_text"], list=list_)
+    return redirect(f"/lists/{list_.id}/")
