65af1b3cab3dd63424e8286a1ab005fb2bf02847
Mistyhops <nedgalkin@gmail.com>
2026-02-18 12:09:27 +0300

    add STATIC_ROOT to settings.py and turned off admin panel

--- superlists/urls.py
+++ superlists/urls.py
@@ -21,7 +21,7 @@ from lists import views as list_views
 from lists import urls as list_urls
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    # path('admin/', admin.site.urls),
     path("", list_views.home_page, name="home"),
     path(f"lists/", include(list_urls)),
 ]
