f1d2b6ac0bb4cac27869f50aa97e2386b45b96cc
Mistyhops <nedgalkin@gmail.com>
2026-02-18 10:37:52 +0300

    add test_layout_and_styling for front css

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -82,7 +82,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
         # Эдит интересно, запомнит ли сайт ее список. Далее она видит, что
         # сайт сгенерировал для нее уникальный URL-адрес – об этом
         # выводится небольшой текст с пояснениями.
-        self.fail('Закончить тест!')
+        # self.fail('Закончить тест!')
 
         # Она посещает этот URL-адрес – ее список по-прежнему там.
         # Удовлетворенная, она снова ложится спать
@@ -169,6 +169,33 @@ class NewVisitorTest(StaticLiveServerTestCase):
         self.assertIn('Купить молоко', page_text)
 
         # Удовлетворенные, они оба ложатся спать
+
+    def test_layout_and_styling(self):
+        """Тест макета и стилевого оформления"""
+        # Эдит открывает домашнюю страницу
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024, 768)
+
+        # Она замечает, что поле ввода аккуратно центрировано
+        inputbox = self.browser.find_element(By.ID, 'id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta=10,
+        )
+
+        # Она начинает новый список и видит, что поле ввода там тоже
+        # аккуратно центрировано
+        inputbox.send_keys('testing')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: testing')
+
+        inputbox = self.browser.find_element(By.ID, 'id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta=10,
+        )
 
 
 if __name__ == '__main__':
